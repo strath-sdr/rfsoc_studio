c31ad36ff77d95c299c69c53b9321aade8ddd3ca
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2021-01-15 16:57:09 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,29 +6,29 @@ from setuptools import find_packages, setup
 
 # global variables
 board = os.environ['BOARD']
-board_notebooks_dir = os.environ['PYNQ_JUPYTER_NOTEBOOKS']
-hw_data_files = []
+nb_dir = os.environ['PYNQ_JUPYTER_NOTEBOOKS']
+hw_files = []
 
 # check whether board is supported
 def check_env():
     if not os.path.isdir(f'boards/{board}'):
         raise ValueError("Board {} is not supported.".format(board))
-    if not os.path.isdir(board_notebooks_dir):
+    if not os.path.isdir(nb_dir):
         raise ValueError(
-            "Directory {} does not exist.".format(board_notebooks_dir))
+            "Directory {} does not exist.".format(nb_dir))
 
 # copy overlays to python package
 def copy_overlay():
     src_dir = os.path.join(f'boards/{board}', 'rfstudio', 'overlay')
     dst_dir = os.path.join('rfstudio')
     copy_tree(src_dir, dst_dir)
-    hw_data_files.extend(
+    hw_files.extend(
         [os.path.join("..", dst_dir, f) for f in os.listdir(dst_dir)])
 
 # copy unique notebooks to jupyter home
 def copy_unique_notebooks():
     src_dir = os.path.join(f'boards/{board}/notebooks')
-    dst_dir = os.path.join(board_notebooks_dir, 'rfstudio', 'board_notebooks')
+    dst_dir = os.path.join(nb_dir, 'rfstudio', 'board_notebooks')
     if os.path.exists(dst_dir):
         shutil.rmtree(dst_dir)
     copy_tree(src_dir, dst_dir)
@@ -36,7 +36,7 @@ def copy_unique_notebooks():
 # copy notebooks to jupyter home
 def copy_notebooks():
     src_dir = os.path.join(f'notebooks')
-    dst_dir = os.path.join(board_notebooks_dir, 'rfstudio', 'common_notebooks')
+    dst_dir = os.path.join(nb_dir, 'rfstudio', 'common_notebooks')
     if os.path.exists(dst_dir):
         shutil.rmtree(dst_dir)
     copy_tree(src_dir, dst_dir)
@@ -52,10 +52,14 @@ setup(
     install_requires=[
         'pynq==2.6',
         'plotly==4.5.2',
+        'rfsoc_sam==1.0',
+    ],
+    dependency_links=[
+      'git+https://github.com/dnorthcote/rfsoc_sam_private',
     ],
     author="David Northcote",
     packages=find_packages(),
     package_data={
-        '': hw_data_files,
+        '': hw_files,
     },
     description="University of Strathclyde RFSoC Studio.")
