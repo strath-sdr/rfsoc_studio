7e50eef306acb0ce99c93599f64f7e49a2c2fe11
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2021-03-01 15:53:41 +0000

    Final setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 import os
 import shutil
-import subprocess
-import sys
 from distutils.dir_util import copy_tree
 
 from setuptools import find_packages, setup
@@ -23,11 +21,18 @@ check_env()
 
 setup(
     name=package_name,
-    version='0.1',
+    version='0.1.0',
     install_requires=[
         'plotly==4.5.2',
         'pynq==2.6',
-        'rfsoc-sam=="0.2.2" @ git+https://github.com/strath-sdr/rfsoc_sam@v0.2.2'
+        'rfsoc-sam @ git+https://github.com/strath-sdr/rfsoc_sam@v0.2.2',
+        'rfsoc-freqplan @ git+https://github.com/strath-sdr/rfsoc_frequency_planner@v0.1.0',
+        'rfsoc-ofdm @ git+https://github.com/strath-sdr/rfsoc_ofdm@v0.2.0',
+        'rfsoc-qpsk @ git+https://github.com/strath-sdr/rfsoc_qpsk@v1.3.0',
+        'rfsoc-radio @ git+https://github.com/strath-sdr/rfsoc_radio@v0.1.0',
+        'pynq-agc @ https://github.com/strath-sdr/pynq_agc/releases/download/v0.3/pynq_agc.tar.gz',
+        'pystrath-dsp @ https://github.com/strath-sdr/dsp_notebooks@v0.1.0',
+        'pystrath-rfsoc @ https://github.com/strath-sdr/rfsoc_notebooks@v0.1.0
     ],
     author="David Northcote",
     packages=find_packages(),
