419c84c32e5408c2866fbb3dcc928cf44dcefd95
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2021-03-01 14:24:44 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+import subprocess
+import sys
 from distutils.dir_util import copy_tree
 
 from setuptools import find_packages, setup
@@ -17,14 +19,15 @@ def check_env():
     if board not in ['RFSoC2x2', 'ZCU111']:
         raise ValueError("Board {} is not supported.".format(board))
 
+def install(package, version):
+    subprocess.check_call([sys.executable, "-m", "pip3", "install", "--force-update", "--no-deps", "https://github.com/strath-sdr/{package}@{version}"])
+
 check_env()
+install("rfsoc_sam", "v0.2.2")
 
 setup(
     name=package_name,
     version='0.1',
-    install_requires=[
-        'rfsoc-sam @ git+https://github.com/strath-sdr/rfsoc_sam@v0.2.2#egg=rfsoc-sam-0.2.2'
-    ],
     author="David Northcote",
     packages=find_packages(),
     package_data={
